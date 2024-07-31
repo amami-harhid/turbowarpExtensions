9c368ed3f17b3395fa8b1edb583e51cfd8510f1e
amami-harhid <amami-thida100@outlook.jp>
2024-07-31 17:12:06 +0900

    update

--- _02_extension/Extension.js
+++ _02_extension/Extension.js
@@ -48,7 +48,7 @@
         ],
     }
     const HOST = 'http://127.0.0.1:5500';
-    const DIRECTORY = 'turbowarpGithub/turbowarpExtensions/_02_extension';
+    const DIRECTORY = 'turbowarpExtensions/_02_extension';
     const FILE = 'sub.js';
     class MyExtension {
         getInfo() {
